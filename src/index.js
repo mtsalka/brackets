56557c59274ab72d0df6c4179163e80e81135ca9
mtsalka <maryia.miskevich@gmail.com>
2020-04-23 15:50:23 +0300

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const conform = new Map;
+  const stack = [];
+  const brackets = str.split('');
+  bracketsConfig.forEach(brackets => {
+    conform.set(`${brackets[1]}`, `${brackets[0]}`);
+  });
+  brackets.forEach(bracket => {
+      if (stack.length > 0 && conform.get(bracket) === stack[stack.length - 1]) {
+        stack.pop();
+      }else{
+        stack.push(bracket);
+      }
+  });
+  return stack.length === 0;
 }
